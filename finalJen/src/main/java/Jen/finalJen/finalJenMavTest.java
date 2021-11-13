33303eddcc011ce7a479d37ef2de48e6119b1e1e
Abdul Manan <i190500@nu.edu.pk>
2021-11-13 22:13:27 +0500

    Final Maven and Jenkin Test

---
+++ finalJen/src/main/java/Jen/finalJen/finalJenMavTest.java
@@ -0,0 +1,10 @@
+package Jen.finalJen;
+
+public class finalJenMavTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("Finally Maven and Jenkins Completed");
+	}
+
+}
