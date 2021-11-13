d35ca191e14e1092313cafd219b4f72aa091c429
Abdul Manan <i190500@nu.edu.pk>
2021-11-13 15:17:56 +0500

    Initial Commit Jenkins

---
+++ jenkinsDemo/src/main/java/jenkinsDemo/jenTest.java
@@ -0,0 +1,10 @@
+package jenkinsDemo;
+
+public class jenTest {
+
+	public void main (String args[]) {
+		
+		System.out.println("Hello CI/CD Jenkins");
+		System.out.println("I am Ready to go");
+	}
+}
